1a7fb671fbe30301bc097b41384faa37f340a410
Michael Humpherys <michaelhumpherys@Michaels-MacBook-Air.local>
2021-03-27 14:46:33 -0400

    models and api routes created and tested

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-//const routes = require('./controllers/');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
@@ -30,8 +30,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use(routes);
+app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
